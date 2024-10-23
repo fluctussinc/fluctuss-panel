d470295f6906f8cdd322aa4ac3a9662c8262cb94
fluctussinc <fluctus.inc@gmail.com>
2024-10-23 20:18:20 +0530

    Add files via upload
    example

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include('admin/track.php'); ?> 
+
+<html>
+    <p style="text-align: center">Example of a index file with track php integrated you can also edit php.ini to achieve the same effect</p>
+    </html>
